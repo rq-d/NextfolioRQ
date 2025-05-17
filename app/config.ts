6523546d5450eaf1cd04ee9925dd6795855bfe7d
Raul Quintana <raulybills@gmail.com>
2025-05-17 17:38:56 -0400

    rm metadata from link

--- app/config.ts
+++ app/config.ts
@@ -1,10 +1,10 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Portfolio",
+  title: "Raul's Portfolio",
   name: "Sirius",
-  ogImage: "/opengraph-image.png",
-  description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+  // ogImage: "/opengraph-image.png",
+  // description:
+    // "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
