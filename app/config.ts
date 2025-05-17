c731c74724ebb2c15d2d36d6f3e4ced0c62619aa
Raul Quintana <raulybills@gmail.com>
2025-05-17 17:42:08 -0400

    Description was necesarry in metadata

--- app/config.ts
+++ app/config.ts
@@ -2,9 +2,9 @@ export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
   title: "Raul's Portfolio",
   name: "Sirius",
-  // ogImage: "/opengraph-image.png",
-  // description:
-    // "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+  ogImage: "/opengraph-image.png",
+  description:
+    "RQ",
 };
 
 export const socialLinks = {
